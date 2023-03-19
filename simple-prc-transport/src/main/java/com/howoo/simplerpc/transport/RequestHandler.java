f3d385c1c68342d077196104900afc505477a55a
sone1208 <449139777@qq.com>
2023-03-19 17:11:20 +0800

    Delete simple-prc-transport directory

--- simple-prc-transport/src/main/java/com/howoo/simplerpc/transport/RequestHandler.java
+++
@@ -1,9 +0,0 @@
-package com.howoo.simplerpc.transport;
-
-import java.io.InputStream;
-import java.io.OutputStream;
-
-//处理网络请求的handler
-public interface RequestHandler {
-    void onRequest(InputStream recive, OutputStream toResp);
-}
